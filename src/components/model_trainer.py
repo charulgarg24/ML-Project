bf37abf1b53091ab7aa6790276316802a07e07dc
charul <charulgarg24@gmail.com>
2024-04-23 19:52:32 +0530

    hyper parameter tunning done

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -46,21 +46,60 @@ class  ModelTrainer:
                 test_array[:,-1]
             )
             models={
-                "Random_Forest" : RandomForestRegressor(),
+                "Random Forest" : RandomForestRegressor(),
                 "Decision Tree" : DecisionTreeRegressor(),
                 "Linear Regression": LinearRegression(),
                 "Gradient Boosting" : GradientBoostingRegressor(),
-                "AdaBoost Classifier" : AdaBoostRegressor(),
-                "K-Neighbours Classifier" : KNeighborsRegressor(),
-                "XGBClassifier": XGBRegressor(),
-                "CatBoosting Classifier" :  CatBoostRegressor(verbose=False),
+                "AdaBoost Regressor" : AdaBoostRegressor(),
+                # "K-Neighbours Classifier" : KNeighborsRegressor(),
+                "XGBRegressor": XGBRegressor(),
+                "CatBoosting Regressor" :  CatBoostRegressor(verbose=False),
             }
                     # now we will see which modelis performing well 
 
                     # we will define this evaluate fucntion in utils.py
+            # here we ahve addedd the parameter for hyperparamter tunning
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
+            }
+        
 
             model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,  
-                                             models=models)
+                                             models=models,param=params)  # in this we are passing the parameter
             
             ## To get the best model score from dict
             best_model_score=max(sorted(model_report.values()))
--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import pandas as pd
 import dill
 from sklearn.metrics import r2_score
 from src.exception import CustomException
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path,obj):
     try:
@@ -17,13 +18,19 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
 
-def evaluate_models(X_train, y_train,X_test,y_test,models):
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
 
         for i in range(len(list(models))):   # it will iterate over each adne very model
             model = list(models.values())[i]
-            model.fit(X_train,y_train) # fir on x_trainy,y_train
+            para=param[list(models.keys())[i]]
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+            # model.fit(X_train,y_train) # fir on x_trainy,y_train
 
             #model.fit(X_train, y_train)  # Train model
 
